c25c7fa05eeb31ddc791a1797855f18b461d9cab
ManuJRS <marejon@thepalacecompany.com>
2026-01-15 17:37:24 -0600

    feat: :art: new forEach excersices

--- src/views/ArrayMetods.vue
+++ src/views/ArrayMetods.vue
@@ -17,8 +17,62 @@ numbers.map((num) => {
 const double = numbers.map((num)=>{
     return num * 2
 })
-
 console.log(double)
+
+// Sumar Valores
+
+type Products = {
+    name: string
+    price: number
+}
+
+const products: Products []= [
+{ 
+    name: "Mouse", 
+    price: 300 
+},
+{ 
+    name: "Keyboard", 
+    price: 700 
+}
+]
+
+let totalProd: number = 0
+
+products.forEach((num: Products)=> {
+    totalProd += num.price
+})
+console.log(totalProd)
+
+// completar tareas
+
+type Tasks = {
+    title: string
+    completed: boolean
+}
+
+const tasks: Tasks [] = [
+  { title: "Login", completed: false },
+  { title: "Fetch data", completed: false }
+]
+
+tasks.forEach((task: Tasks)=> {
+    task.completed = true
+})
+
+console.log(tasks)
+
+// sumar datos
+
+const prices: number[] = [100, 250, 50]
+
+let sumPrices: number = 0
+
+prices.forEach((price: number)=> {
+    sumPrices += price
+})
+
+console.log(sumPrices)
 
 </script>
 
