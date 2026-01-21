4ecb7e072cd1d5958389107e2a3f02b521d5ec41
ManuJRS <marejon@thepalacecompany.com>
2026-01-20 18:41:00 -0600

    chore: :art: i do a new models excersice with vue

---
+++ src/components/UseCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { UserModel } from '@/domain/UserModel';
+import type { UserDto } from '@/domain/user.dto';
+import { ref } from 'vue';
+
+const showFullEmail = ref(false)
+
+
+const userDto: UserDto = {
+    id: 1,
+    name: "Manuel",
+    email: "test@correo.com",
+    isActive: true
+}
+
+const user = new UserModel(userDto)
+
+</script>
+
+<template>
+    <h1>Esta es una card</h1>
+    <p>ID: {{ user.id }}</p>
+    <p>NAME: {{ user.displayName }}</p>
+    <p>EMAIL: {{ showFullEmail ? user.fullName : user.maskedEmail }}</p>
+    <button @click="showFullEmail = !showFullEmail" class=" border rounded px-2 hover:cursor-pointer my-4 ">
+        {{ showFullEmail ? "Ocultar Email" : "Mostrar Email" }}
+    </button>
+    <p>ACTIVO: {{ user.statusLabel }}</p>
+</template>
