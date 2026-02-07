2114ac4128c528223797d817ca350fd7ecf17032
ManuJRS <marejon@thepalacecompany.com>
2026-02-06 18:49:48 -0600

    feat(props): :art: excersice props practices

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+    Title: string
+    Price: number
+    InStockL: boolean
+}>()
+
+
+
+</script>
+
+<template>
+    <h1>cards product</h1>
+    {{ props.Title }}
+    {{ props.Price }}
+    {{ props.InStockL }}
+
+    <p v-if="props.InStockL">Disponible</p>
+    <p v-else>Agotado</p>
+    <p v-if="Price===0">Gratis</p>
+</template>
